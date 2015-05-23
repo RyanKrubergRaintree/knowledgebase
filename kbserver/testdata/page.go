ffdbf193364d7f85d7c70606ad7a54263c5c747b
Egon Elbre <egonelbre@gmail.com>
2015-05-23 17:30:45 +0300

    Remove dependency.

--- kbserver/testdata/page.go
+++ kbserver/testdata/page.go
@@ -1,9 +1,6 @@
 package testdata
 
 import (
-	"math/rand"
-
-	lorem "github.com/drhodes/golorem"
 	"github.com/raintreeinc/knowledgebase/kb"
 )
 
@@ -12,7 +9,7 @@ func NewPage(owner string, title string) *kb.Page {
 		Owner:    kb.Slugify(owner),
 		Slug:     kb.Slugify(owner + ":" + title),
 		Title:    title,
-		Synopsis: lorem.Paragraph(1, 1),
+		Synopsis: "Welcome to a test page.",
 		Story: kb.Story{
 			kb.Tags("Welcome", "Home", "Some Example"),
 			kb.Paragraph("Link to self: [[Community:Welcome]]."),
@@ -21,15 +18,7 @@ func NewPage(owner string, title string) *kb.Page {
 			kb.Paragraph("kbgroup: [[group:Groups]], [[group:Community]], [[group:Community Details]]"),
 			kb.Paragraph("kbtag: [[tag:Tags]], [[tag:Home]]"),
 			kb.Paragraph("kbuser: [[user:Egon Elbre]]"),
-			kb.Paragraph(lorem.Paragraph(1, 1)),
-			kb.Paragraph(lorem.Paragraph(1, 1)),
-			kb.Paragraph(lorem.Paragraph(1, 1)),
 		},
-	}
-
-	N := rand.Intn(5)
-	for i := 0; i < N; i++ {
-		page.Story.Append(kb.Paragraph(lorem.Paragraph(1, 1)))
 	}
 
 	return page
