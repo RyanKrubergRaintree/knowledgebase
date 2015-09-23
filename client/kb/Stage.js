39a36aed09bcfafbeb9b5ff78827c683a71072a6
Egon Elbre <egonelbre@gmail.com>
2015-09-23 10:36:10 +0300

    Fix closing page.

--- client/kb/Stage.js
+++ client/kb/Stage.js
@@ -1,4 +1,4 @@
-package('kb', function(exports){
+package('kb', function(exports) {
 	'use strict';
 
 	depends('util/Notifier.js');
@@ -7,56 +7,59 @@ package('kb', function(exports){
 
 	depends('util/ParseJSON.js');
 
-	function success(xhr){
+	function success(xhr) {
 		return (200 <= xhr.status) && (xhr.status < 300);
 	}
 
-	function bindready(xhr, fn, self){
-		return function(){
-			if(xhr.readyState !== 4){ return; }
+	function bindready(xhr, fn, self) {
+		return function() {
+			if (xhr.readyState !== 4) {
+				return;
+			}
 			fn.call(self, xhr);
 		}
 	}
 
-	function Editing(stage){
+	function Editing(stage) {
 		this.stage = stage;
 		this.items = {};
 	}
 	Editing.prototype = {
-		start: function(id){
+		start: function(id) {
 			this.items[id] = true;
 			this.pack();
 			this.stage.changed();
 		},
-		stop: function(id){
+		stop: function(id) {
 			delete this.items[id];
 			this.pack();
 			this.stage.changed();
 		},
 		// removes all non-existing items
-		pack: function(){
+		pack: function() {
 			var t = {};
 			var page = this.stage.page;
-			for(var i = 0; i < page.story.length; i++){
+			for (var i = 0; i < page.story.length; i++) {
 				var item = page.story[i];
-				if(this.items[item.id]){
+				if (this.items[item.id]) {
 					t[item.id] = true;
 				}
 			}
 			this.items = t;
 		},
-		clear: function(){
+		clear: function() {
 			this.items = {};
 			this.changed();
 		},
-		item: function(id){
+		item: function(id) {
 			return this.items[id];
 		}
 	};
 
 	// Stage represents a staging area where modifications/loading are done.
 	exports.Stage = Stage;
-	function Stage(ref, page){
+
+	function Stage(ref, page) {
 		this.id = GenerateID();
 
 		this.creating = (ref.url === null) || (ref.url === '');
@@ -87,53 +90,66 @@ package('kb', function(exports){
 	}
 
 	Stage.prototype = {
-		close: function(){
-			this.state = 'closed';
+		close: function() {
 			this.changed();
-			this.notifier.handle({type: 'closed', stage: this});
+			this.notifier.handle({
+				type: 'closed',
+				stage: this
+			});
 		},
-		changed: function(){
-			this.notifier.emit({type: 'changed', stage: this});
+		changed: function() {
+			this.notifier.emit({
+				type: 'changed',
+				stage: this
+			});
 		},
-		urlChanged: function(){
-			this.notifier.emit({type: 'urlChanged', stage: this});
+		urlChanged: function() {
+			this.notifier.emit({
+				type: 'urlChanged',
+				stage: this
+			});
 		},
 
-		wideChanged: function(){
-			this.notifier.emit({type: 'widthChanged', stage: this});
+		wideChanged: function() {
+			this.notifier.emit({
+				type: 'widthChanged',
+				stage: this
+			});
 		},
-		expand: function(){
+		expand: function() {
 			this.wide = true;
 			this.wideChanged();
 		},
-		collapse: function(){
+		collapse: function() {
 			this.wide = false;
 			this.wideChanged();
 		},
 
-		canCreate: function(){
+		canCreate: function() {
 			return this.allowed.indexOf('PUT') >= 0;
 		},
-		canModify: function(){
+		canModify: function() {
 			return this.allowed.indexOf('POST') >= 0;
 		},
-		canDestroy: function(){
+		canDestroy: function() {
 			return this.allowed.indexOf('DELETE') >= 0;
 		},
 
-		updateStatus_: function(xhr){
+		updateStatus_: function(xhr) {
 			var allowed = xhr.getResponseHeader('Allow');
-			if(typeof allowed === 'string'){
-				this.allowed = allowed.split(',').map(function(v){ return v.trim(); });
+			if (typeof allowed === 'string') {
+				this.allowed = allowed.split(',').map(function(v) {
+					return v.trim();
+				});
 			}
 
 			var ok = success(xhr);
 			this.state = 'loaded';
-			if(!ok){
+			if (!ok) {
 				this.state = 'error';
-				if(xhr.status === 404){
+				if (xhr.status === 404) {
 					this.state = 'not-found';
-					if(this.canCreate()){
+					if (this.canCreate()) {
 						this.creating = true;
 					}
 				}
@@ -146,8 +162,10 @@ package('kb', function(exports){
 			return ok;
 		},
 
-		patch: function(op){
-			if((this.url === null)||(this.url === '')){ return; }
+		patch: function(op) {
+			if ((this.url === null) || (this.url === '')) {
+				return;
+			}
 
 			// var version = this.page.version;
 			this.page.apply(op);
@@ -157,10 +175,12 @@ package('kb', function(exports){
 
 			this.changed();
 		},
-		nextPatch_: function(){
-			if(this.patching_){ return; }
+		nextPatch_: function() {
+			if (this.patching_) {
+				return;
+			}
 			var patch = this.patches_.shift();
-			if(patch){
+			if (patch) {
 				this.patching_ = true;
 
 				var xhr = new XMLHttpRequest();
@@ -175,10 +195,10 @@ package('kb', function(exports){
 				xhr.send(JSON.stringify(patch));
 			}
 		},
-		patchDone_: function(xhr){
+		patchDone_: function(xhr) {
 			this.patching_ = false;
 
-			if(!this.updateStatus_(xhr)){
+			if (!this.updateStatus_(xhr)) {
 				//TODO: don't drop changes in case of errors
 				this.patches_ = [];
 				this.patching_ = false;
@@ -187,17 +207,19 @@ package('kb', function(exports){
 			}
 			this.nextPatch_();
 		},
-		patchError_: function(/* ev */){
+		patchError_: function( /* ev */ ) {
 			this.patches_ = [];
 			this.patching_ = false;
 			this.pull();
 		},
 
-		refresh: function(){
+		refresh: function() {
 			this.pull();
 		},
-		pull: function(){
-			if((this.url === null)||(this.url === '')){ return; }
+		pull: function() {
+			if ((this.url === null) || (this.url === '')) {
+				return;
+			}
 
 			this.state = 'loading';
 			this.changed();
@@ -210,16 +232,16 @@ package('kb', function(exports){
 			xhr.setRequestHeader('Accept', 'application/json');
 			xhr.send();
 		},
-		pullDone_: function(xhr){
-			if(!this.updateStatus_(xhr)){
+		pullDone_: function(xhr) {
+			if (!this.updateStatus_(xhr)) {
 				this.changed();
 				return;
 			}
 
 			var data = kb.util.ParseJSON(xhr.responseText),
-			page = new kb.Page(data);
-			if(xhr.responseURL){
-				if(this.url !== xhr.responseURL){
+				page = new kb.Page(data);
+			if (xhr.responseURL) {
+				if (this.url !== xhr.responseURL) {
 					this.url = xhr.responseURL;
 					this.urlChanged();
 				}
@@ -232,7 +254,7 @@ package('kb', function(exports){
 
 			kb.TrackPageView(this.url, this.page.title);
 		},
-		pullError_: function(/* ev */){
+		pullError_: function( /* ev */ ) {
 			this.state = 'failed';
 			this.lastStatus = 'failed';
 			this.lastStatusText = '';
@@ -240,8 +262,10 @@ package('kb', function(exports){
 			this.changed();
 		},
 
-		create: function(){
-			if(!this.creating){ return; }
+		create: function() {
+			if (!this.creating) {
+				return;
+			}
 			this.url = '/' + this.link;
 			this.urlChanged();
 
@@ -255,16 +279,19 @@ package('kb', function(exports){
 			xhr.send(JSON.stringify({
 				title: this.title,
 				slug: this.link,
-				story: [
-					{ id: GenerateID(), type: 'tags' },
-					{ id: GenerateID(), type: 'factory'}
-				]
+				story: [{
+					id: GenerateID(),
+					type: 'tags'
+				}, {
+					id: GenerateID(),
+					type: 'factory'
+				}]
 			}));
 
 			this.changed();
 		},
-		createDone_: function(xhr){
-			if(!this.updateStatus_(xhr)){
+		createDone_: function(xhr) {
+			if (!this.updateStatus_(xhr)) {
 				this.changed();
 				return;
 			}
@@ -272,7 +299,7 @@ package('kb', function(exports){
 			this.state = 'created';
 			this.refresh();
 		},
-		createError_: function(/* ev */){
+		createError_: function( /* ev */ ) {
 			this.state = 'failed';
 			this.lastStatus = 'failed';
 			this.lastStatusText = '';
@@ -280,8 +307,10 @@ package('kb', function(exports){
 			this.changed();
 		},
 
-		destroy: function(){
-			if((this.url === null)||(this.url === '')){ return; }
+		destroy: function() {
+			if ((this.url === null) || (this.url === '')) {
+				return;
+			}
 
 			var xhr = new XMLHttpRequest();
 			xhr.open('DELETE', this.url, true);
@@ -292,12 +321,12 @@ package('kb', function(exports){
 			xhr.send();
 			return xhr;
 		},
-		destroyDone_: function(ev){
+		destroyDone_: function(ev) {
 			this.updateStatus_(ev.target);
 			this.changed();
 			this.pull();
 		},
-		destroyError_: function(/* ev */){
+		destroyError_: function( /* ev */ ) {
 			this.state = 'failed';
 			this.lastStatus = 'failed';
 			this.lastStatusText = '';
