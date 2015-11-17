25bc87b82c4ae277eb3c676730f18791987b9ef2
Egon Elbre <egonelbre@gmail.com>
2015-11-17 15:27:38 +0200

    Split the login into two.

--- client/Login.js
+++ client/Login.js
@@ -4,17 +4,85 @@ package("kb.boot", function(exports) {
 	depends("app.css");
 	depends("Login.css");
 
-	exports.Login = React.createClass({
+	var Guest = React.createClass({
 		getInitialState: function() {
 			return {
 				processing: false,
 				failure: false
 			};
 		},
-		loginCustomer: function(ev) {
+		login: function(ev) {
 			ev.preventDefault();
-			//
+
+			var username = this.refs.username.value;
+			var password = this.refs.password.value;
+
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = this.change;
+			xhr.onerror = this.error;
+			xhr.open("POST", "/system/auth/guest");
+
+			var form = new FormData();
+			form.append("user", username);
+			form.append("code", password);
+
+			xhr.send(form);
 		},
+		change: function(ev) {
+			console.log(ev);
+		},
+		error: function(ev) {
+			console.log(ev);
+		},
+		render: function() {
+			return React.DOM.form({
+					className: "logins",
+					onSubmit: this.login
+				},
+				React.DOM.table(null, React.DOM.tbody(null,
+					React.DOM.tr(null,
+						React.DOM.td(null, React.DOM.label({
+							htmlFor: "username"
+						}, "Username:")),
+						React.DOM.td(null, React.DOM.input({
+							ref: "username",
+							name: "username",
+							tabIndex: 1
+						})),
+						React.DOM.td({
+								rowSpan: 2
+							},
+							React.DOM.input({
+								className: "button",
+								type: "submit",
+								value: "Login",
+								tabIndex: 3
+							}))
+					), React.DOM.tr(null,
+						React.DOM.td(null, React.DOM.label({
+							htmlFor: "password"
+						}, "Password:")),
+						React.DOM.td(null, React.DOM.input({
+							ref: "password",
+							name: "password",
+							type: "password",
+							tabIndex: 2
+						}))
+					)
+				)));
+		}
+	});
+
+	var Google = React.createClass({
+		render: function() {
+			return React.DOM.a({
+				className: "button",
+				href: "#"
+			}, "Google");
+		}
+	});
+
+	exports.Login = React.createClass({
 		render: function() {
 			return React.DOM.div({
 					id: "login"
@@ -27,65 +95,14 @@ package("kb.boot", function(exports) {
 				}, React.DOM.div({
 						className: "modal"
 					},
-					React.DOM.form({
-							className: "logins",
-							onSubmit: this.loginCustomer
-						},
-						React.DOM.h2(null, "Customer Login:"),
-						React.DOM.table(null, React.DOM.tbody(null,
-							React.DOM.tr(null,
-								React.DOM.td(null, React.DOM.label({
-									htmlFor: "username"
-								}, "Username:")),
-								React.DOM.td(null, React.DOM.input({
-									name: "username",
-									tabIndex: 1
-								})),
-								React.DOM.td({
-										rowSpan: 2
-									},
-									React.DOM.input({
-										className: "button",
-										type: "submit",
-										value: "Login",
-										tabIndex: 3
-									}))
-							), React.DOM.tr(null,
-								React.DOM.td(null, React.DOM.label({
-									htmlFor: "password"
-								}, "Password:")),
-								React.DOM.td(null, React.DOM.input({
-									name: "password",
-									type: "password",
-									tabIndex: 2
-								}))
-							)
-						)),
-						React.DOM.div({
-								className: "logins"
-							},
-							React.DOM.h2(null, "Employee Login:"),
-							React.DOM.a({
-								className: "button",
-								href: "#"
-							}, "Google")
-						)
-					)
+					React.DOM.h2(null, "Customer Login:"),
+					React.createElement(Guest, {
+						url: "/system/auth/guest"
+					}),
+					React.DOM.h2(null, "Employee Login:"),
+					React.createElement(Google)
 				))
 			);
 		}
 	});
 });
-
-
-/*
-Knowledge Base works best with Google Chrome. Other browsers still have some known issues.
-
-Customer login:
-
-Username:
-Login
-Password:
-Raintree employee login:
-
-Google*/
