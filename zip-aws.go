2e46f69e0a9a752a61ee8c11b871d25428eed990
ToomasA <61152814+ToomasA@users.noreply.github.com>
2023-11-22 11:30:30 +0200

    nginx changes to allow bigger uploads

--- zip-aws.go
+++ zip-aws.go
@@ -41,7 +41,7 @@ func run(name string, args ...string) error {
 func build() {
 	check(run("go", "build", "-v", "-o", filepath.Join(".bin", "run"), "."))
 	AddDir(".bin")
-	AddDir(".ebextensions")
+	AddDir(".platform")
 
 	AddGlob("*.json")
 	AddGlob("Docker*")
